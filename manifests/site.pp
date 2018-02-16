4d1ca130cef5c7c6e7d615b5cba8a40e645a6fa1
Thomas Uphill <thomas@narrabilis.com>
2018-02-15 20:58:01 -0800

    adding mysql stuff

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,3 +1,11 @@
+node cookbook {
+  $mysql_password='TopSecret'
+  file { '/usr/local/bin/backup-mysql':
+    content => template('admin/backup-mysql.sh.erb'),
+    mode    => '0755',
+  }
+}
+
 node default {
   include base
   include puppet
