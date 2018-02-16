6e402a3d83a6703744017eea6a3be38f54057f90
Thomas Uphill <thomas@narrabilis.com>
2018-02-15 21:04:29 -0800

    addresslist

--- manifests/site.pp
+++ manifests/site.pp
@@ -4,6 +4,10 @@ node cookbook {
     content => template('admin/backup-mysql.sh.erb'),
     mode    => '0755',
   }
+  $ipaddresses = ['192.168.0.1','158.43.128.1', '10.0.75.207' ]
+  file { '/tmp/addresslist.txt':
+    content => template('base/addresslist.erb')
+  }
 }
 
 node default {
