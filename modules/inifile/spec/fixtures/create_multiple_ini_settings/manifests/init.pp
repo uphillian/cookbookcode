66f2ed5fdd2ac3c3c03b0f4924fb0a58e4216cd4
Thomas Uphill <thomas@narrabilis.com>
2018-03-27 21:21:49 -0700

    adding modules to repo

---
+++ modules/inifile/spec/fixtures/create_multiple_ini_settings/manifests/init.pp
@@ -0,0 +1,16 @@
+# Manifest creating multiplr ini_settings
+class create_multiple_ini_settings {
+
+$defaults = { 'path' => '/tmp/foo.ini' }
+$example = {
+  'section1' => {
+    'setting1'  => 'value1',
+    'settings2' => {
+      'ensure' => 'absent'
+    }
+  }
+}
+create_ini_settings($example, $defaults)
+
+}
+
