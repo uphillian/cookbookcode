476bd2cefc50d658d06d1561d165f6c125a2b55b
Thomas Uphill <thomas@narrabilis.com>
2018-04-02 11:28:17 -0700

    myfw

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,15 +1,16 @@
 node cookbook {
-  $mysql_password='TopSecret'
-  file { '/usr/local/bin/backup-mysql':
-    content => template('admin/backup-mysql.sh.erb'),
-    mode    => '0755',
-  }
-  $ipaddresses = ['192.168.0.1','158.43.128.1', '10.0.75.207' ]
-  file { '/tmp/addresslist.txt':
-    content => template('base/addresslist.erb')
-  }
-  $message = secret('/etc/puppetlabs/code/environments/production/modules/admin/files/secret_message.gpg')
-  notify { "The secret message is: ${message}": }
+  #$mysql_password='TopSecret'
+  #file { '/usr/local/bin/backup-mysql':
+  #  content => template('admin/backup-mysql.sh.erb'),
+  #  mode    => '0755',
+  #}
+  #$ipaddresses = ['192.168.0.1','158.43.128.1', '10.0.75.207' ]
+  #file { '/tmp/addresslist.txt':
+  #  content => template('base/addresslist.erb')
+  #}
+  #$message = secret('/etc/puppetlabs/code/environments/production/modules/admin/files/secret_message.gpg')
+  #notify { "The secret message is: ${message}": }
+  include myfw
 }
 
 node webserver {
